eed3385dd78fe433489a640e532edbb502b9009a
Esvaran Arun <esvar2004@gmail.com>
2025-06-07 23:56:00 +0800

    fix: Wallet Error

--- xrpl-js/routers/wallet.py
+++ xrpl-js/routers/wallet.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, HTTPException
 from xrpl.wallet import generate_faucet_wallet
 from services.xrpl_client import client
+from routers.donate import get_faucet_wallet
 
 router = APIRouter()
 
 @router.post("/create")
 def create_wallet():
     try:
-        wallet = generate_faucet_wallet(client, debug=True)
+        wallet = get_faucet_wallet()
         return {
             "wallet": {
                 "address": wallet.classic_address,
